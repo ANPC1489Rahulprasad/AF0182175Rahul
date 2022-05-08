ad11a8e3f7bc2aff14712b9e28373c4b3c6c4c63
ANPC1489Rahulprasad <105111336+ANPC1489Rahulprasad@users.noreply.github.com>
2022-05-08 12:51:44 +0530

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])
+	{
+	    float p = 2, R = 2, N = 2;
+		System.out.println("Simple interest = " + SI);
+		
+	}
+}
+
---
+++ information.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class information{
+ public static void main(string[] args){
+  System.out.println("Hello\nRahul Prasad! \nStream-Computer Science");
+ }
+}
